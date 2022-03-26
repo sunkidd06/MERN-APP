fe0f8e0e133afe6130a31b2b6d5774c651131be4
Dat Vo <datvodat288@gmail.com>
2022-03-26 23:35:54 +0700

    add all file

--- client/src/contexts/constant.js
+++ client/src/contexts/constant.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8000/api' : 'somedeployed';
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://serene-taiga-31326.herokuapp.com/api' : 'somedeployed';
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit';
